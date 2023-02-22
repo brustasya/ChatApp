88fe14f10ab03d423df096e2d45b4cbd43e24544
brustasya <sabobruskina@edu.hse.ru>
2023-02-22 20:46:15 +0300

    Task 1 done

--- ChatApp/AppDelegate.swift
+++ ChatApp/AppDelegate.swift
@@ -9,11 +9,54 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
     var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    var state = "Not Running"
+    let logger: LoggerLogic = Logger()
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        Logger.shared.printLog(log: "Application moved from \(state) to Inactive: \(#function)")
+        state = "Inactive"
+        
         return true
     }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        Logger.shared.printLog(log: #function)
+        
+        return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        Logger.shared.printLog(log: "Application moved from \(state) to Inactive: \(#function)")
+        state = "Inactive"
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        Logger.shared.printLog(log: "Application moved from \(state) to Active: \(#function)")
+        state = "Active"
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        Logger.shared.printLog(log: "Application moved from \(state) to Background: \(#function)")
+        state = "Background"
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        Logger.shared.printLog(log: "Application moved from \(state) to Inactive: \(#function)")
+        state = "Inactive"
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        Logger.shared.printLog(log: "Application moved from \(state) to Not Running: \(#function)")
+        state = "Suspended"
+    }
 }
+
+
 
--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        Logger.shared.printLog(log: "Called method: \(#function)")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        Logger.shared.printLog(log: "Called method: \(#function)")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        Logger.shared.printLog(log: "Called method: \(#function)")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        Logger.shared.printLog(log: "Called method: \(#function)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        Logger.shared.printLog(log: "Called method: \(#function)")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        Logger.shared.printLog(log: "Called method: \(#function)")
+    }
 }
+
 
